bc85ac150c2483535780d55d9a2371455c339827
chiqvu1 <chiquynhvu268@gmail.com>
2019-09-19 11:20:12 -0700

    Changed button text. Created an ArrayList type String of different toast messages to display when button is clicked

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.diceroller
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import java.lang.reflect.Array
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Ready to Rock n Roll"
+        rollButton.text = "Rock 'n' Roll"
         rollButton.setOnClickListener {
             rollDice()
         }
@@ -26,7 +29,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -36,9 +38,21 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
+        val toast_message: ArrayList<String> = ArrayList<String>(6)
+        toast_message.add("Someone will invite you to a Karaoke party.")
+        toast_message.add("May you someday be carbon neutral.")
+        toast_message.add("Never forget a friend. Especially if he owes you.")
+        toast_message.add("Fortune not found? Abort, Retry, Ignore.")
+        toast_message.add("An alien of some sort will be appearing to you shortly.")
+        toast_message.add("You will be hungry again in one hour.")
+
+        Toast.makeText(this, toast_message[randomInt - 1], Toast.LENGTH_LONG).show()
+
         diceImage.setImageResource((drawableResource))
 
-
-
     }
+
+
+
+
 }
